d258259fbbcfd486413583e3a7888a0d5c668b6a
james-julius <jamesdarby7@gmail.com>
2020-11-29 21:16:46 -0800

    Added in dynamic cols and rows

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import { useCallback, useEffect, useState } from 'react';
 function App() {
   const [columns, setColumns] = useState(15);
   const [rows, setRows] = useState(15);
-  const [cellWidth, setCellWidth] = useState(30);
-  const [cellHeight, setCellHeight] = useState(30);
-  const [map, setMap] = useState([
-    [0, 0, 0],
-    [0, 0, 0]
-  ]);
+  const [maxCols, maxRows] = [50, 50];
+  const [cellWidth, setCellWidth] = useState(20);
+  const [cellHeight, setCellHeight] = useState(20);
+  const [map, setMap] = useState([]);
   const [obstacleFrequency, setObstacleFrequency] = useState(0.3);
 
   class Node {
@@ -30,30 +28,59 @@ function App() {
         mapToBe[column][row] = new Node(row, column);
       }
     }
-    console.log('setting map to:' + mapToBe);
     setMap(mapToBe);
-  }, []);
+  }, [columns, rows]);
 
   useEffect(() => {
     mapSetup();
-  }, []);
+  }, [columns, rows]);
 
 
   return (
     <div className="App">
-      <div id="map" style={{height: "auto"}}>
-        {map && map.map((row, idx) => {
-          return <div className="column">
-            {row[idx] && map[idx].map((cell) => {
-              return <>
-                <div 
-                  class={`cell ${cell.class}`} 
-                  style={{height: cellHeight, width: cellWidth}}>
-                </div>
-              </>
-            })}
-          </div>
-        })}
+      <label>
+        Map Columns:
+        <select value={columns} onChange={(e) => setColumns(e.target.value)}>
+          {Array.from(Array(maxCols).keys()).map((val, idx) => {
+            return (
+              <option key={`col-dropdown-${idx}`} value={idx + 1}>
+                {idx + 1}
+              </option>
+            );
+          })}
+        </select>
+      </label>
+      <label>
+        Map Rows:
+        <select value={rows} onChange={(e) => setRows(e.target.value)}>
+          {Array.from(Array(maxRows).keys()).map((val, idx) => {
+            return (
+              <option key={`row-dropdown-${idx}`} value={idx + 1}>
+                {idx + 1}
+              </option>
+            );
+          })}
+        </select>
+      </label>
+      <div id="map" style={{ height: "auto" }}>
+        {map &&
+          map.map((row, idx) => {
+            return (
+              <div className="column">
+                {row[idx] &&
+                  map[idx].map((cell) => {
+                    return (
+                      <>
+                        <div
+                          class={`cell ${cell.class}`}
+                          style={{ height: cellHeight, width: cellWidth }}
+                        ></div>
+                      </>
+                    );
+                  })}
+              </div>
+            );
+          })}
       </div>
     </div>
   );
