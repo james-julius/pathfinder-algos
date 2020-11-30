b0cb23830219dcd594938e52c50605d21d787d34
james-julius <jamesdarby7@gmail.com>
2020-11-29 19:44:49 -0800

    Coding basis for algorithm map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useEffect, useState } from 'react';
 
 function App() {
+  const canvas = useRef('canvas');
+  const [columns, setColumns] = useState(15);
+  const [rows, setRows] = useState(15);
+  const [canvasWidth, setCanvasWidth] = useState(30);
+  const [canvasHeight, setCanvasHeight] = useState(30);
+  const [map, setMap] = useState([]);
+  const [obstacleFrequency, setObstacleFrequency] = useState(0.3);
+
+  class Node {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+      this.neighbours = [];
+      this.previous = null;
+      this.imPassable = Math.random() < obstacleFrequency;
+    }
+  }
+
+  function boardSetup() {
+    for (let column of columns) {
+      map.push([]);
+      for (let row of rows) {
+        map[column][row] = new Node(row, column);
+      }
+    }
+  }
+  useEffect(() => {
+    canvas.getContext("2d");
+    canvas.width = canvasWidth * columns;
+    canvas.height = canvasHeight * rows;
+    boardSetup();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas ref="canvas" id="canvas">
+
+      </canvas>
     </div>
   );
 }
